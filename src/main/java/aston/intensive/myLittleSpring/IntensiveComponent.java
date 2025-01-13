82525df3cbf4b0f17d8e61c996557b43dc771809
Roman Voyttyuk <romavoytyuk@gmail.com>
2025-01-13 16:04:18 +0300

    @IntensiveComponent

---
+++ src/main/java/aston/intensive/myLittleSpring/IntensiveComponent.java
@@ -0,0 +1,8 @@
+package aston.intensive.myLittleSpring;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface IntensiveComponent {
+}

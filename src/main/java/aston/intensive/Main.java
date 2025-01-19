abcf89ea78659c55c8cce30f762244793f18320b
Roman Voyttyuk <romavoytyuk@gmail.com>
2025-01-19 18:49:57 +0300

    Main with test

--- src/main/java/aston/intensive/Main.java
+++ src/main/java/aston/intensive/Main.java
@@ -1,7 +1,17 @@
 package aston.intensive;
 
+import aston.intensive.model.subpackage.AnotherClass;
+import aston.intensive.myLittleSpring.LittleSpringContext;
+
+import java.lang.reflect.InvocationTargetException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        var context = new LittleSpringContext("model");
+        var anotherClassClass = context.getObject(AnotherClass.class);
+        anotherClassClass.go();
+
     }
 }

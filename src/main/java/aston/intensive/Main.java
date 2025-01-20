af61be001cf510734661b9469cdf06616b119d8e
Roman Voyttyuk <romavoytyuk@gmail.com>
2025-01-20 22:02:28 +0300

    name var anotherClass fixed

--- src/main/java/aston/intensive/Main.java
+++ src/main/java/aston/intensive/Main.java
@@ -8,8 +8,8 @@ public class Main {
     public static void main(String[] args) {
 
         var context = new LittleSpringContext("model");
-        var anotherClassClass = context.getObject(AnotherClass.class);
-        anotherClassClass.go();
+        var anotherClass = context.getObject(AnotherClass.class);
+        anotherClass.go();
 
     }
 }
